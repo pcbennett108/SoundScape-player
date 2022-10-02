e93f990c45cbf7d97f57061496046ec9fe912b73
pcbennett108 <pcbennett42@gmail.com>
2022-10-02 11:46:55 -0600

    add error and loading components, map over data array

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -7,7 +7,8 @@ const Discover = () => {
   const { data, isFetching, error } = useGetTopChartsQuery();
   const genreTitle = "Pop";
 
-  console.log(data);
+  if (isFetching) return <Loader title="Loading songs..." />;
+  if (error) return <Error />;
 
   return (
     <div className="flex flex-col">
@@ -27,7 +28,7 @@ const Discover = () => {
         </select>
       </div>
       <div className="flex flex-wrap sm:justify-start justify-center gap-8">
-        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((song, i) => (
+        {data?.map((song, i) => (
           <SongCard key={song.key} song={song} i={i} />
         ))}
       </div>
