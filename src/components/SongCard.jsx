edbcd7b62b5baccd63a54591598747ae76515de4
pcbennett108 <pcbennett42@gmail.com>
2022-10-02 19:39:22 -0600

    add top charts, top artists, and swiper component

--- src/components/SongCard.jsx
+++ src/components/SongCard.jsx
@@ -6,6 +6,7 @@ import { playPause, setActiveSong } from "../redux/features/playerSlice";
 
 const SongCard = ({ song, isPlaying, activeSong, data, i }) => {
   const dispatch = useDispatch();
+
   const handlePauseClick = () => {
     dispatch(playPause(false));
   };
